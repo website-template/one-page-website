3db75f8e3ddd219fa9dd766916d651d160f166ab
Mohammad ALshehri <moh4sa@users.noreply.github.com>
2020-07-09 15:10:02 +0300

    Improvements

--- assets/js/main.js
+++ assets/js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Mohammed Alshehri.", "Developer."],
+      strings: ["Mohammed Alshehri.", "a Developer."],
       typeSpeed: 100,
       loop: true,
     });
